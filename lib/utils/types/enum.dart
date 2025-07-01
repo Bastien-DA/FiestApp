7cb625027bdaf1de4b5121f169f2e36b6678c584
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2025-07-01 12:09:25 +0200

    [FIX] alcoolConsumtion enum

--- lib/utils/types/enum.dart
+++ lib/utils/types/enum.dart
@@ -2,6 +2,6 @@ enum PollChoiceStatus { none, selected, validated }
 
 enum WhoCardType { drive, sleep }
 
-enum AlcoholConsumption { occasionally, regularly, seasoned }
+enum AlcoholConsumption { Occasional, Regular, Veteran }
 
 enum InputType { text, number, date, time, counter }
